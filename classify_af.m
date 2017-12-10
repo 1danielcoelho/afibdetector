687110d579533cfa4064ddc9203b06b47b051449
Daniel Coelho <daniel.coelho@navegatium.com>
2017-12-10 16:51:14 -0200

    Transform main script into a function

--- classify_af.m
+++ classify_af.m
@@ -1,14 +1,4 @@
-%%TODO
-% Delay separating training sets from testing sets as much as possible
-% Maybe classify between AFIB and normal within the same series (i.e. use
-% first 3 hours to train, other 7 to test --> Need to screen for records
-% with many episodes)
-
-%% Cleanup
-clc
-clear all
-close all
-
+function [ results ] = classify_af( classifier, trainingPercentage, numRecordsToClassify, numPrincipalComponents )
 %% Import data
 disp('Importing data...');
 
@@ -27,14 +17,11 @@ recordNames = {'04015', '04043', '04048', '04126', '04746', '04908', ...
                '04936', '05091', '05121', '05261', '06426', '06453', ...
                '06995', '07162', '07859', '07879', '07910', '08215', ...
                '08219', '08378', '08405', '08434', '08455'};
-
-% Each file has two leads
-numRecords = length(recordNames) * 2;
            
 % Loads all the records into a 'records' structure
-for recIndex = 1:length(recordNames)
+for recIndex = 1:min(numRecordsToClassify, length(recordNames))
     recName = char(recordNames(recIndex));
-    disp(recName);
+    %disp(recName);
     recPath = strcat('mit-bih\', recName);    
     
     % Actually read everything    
@@ -56,14 +43,14 @@ cd(prev_folder);
 clear ecg_signal
 
 %% Break record signals into windows
-disp('Separating ECG1 into windows and extracting their classes...');
+disp('Separating signals into windows and extracting their classes...');
 windowSizeSeconds = 4;
 discardMixedWindows = 1; %Whether we keep windows that are only partially afib/normal
 
 recordNames = fieldnames(records);
 for recordName=recordNames'    
     record = records.(recordName{1});
-    disp(recordName{1});
+    %disp(recordName{1});
     
     samplingFreq = record.Fs;
     PSDSize = windowSizeSeconds * samplingFreq;
@@ -120,7 +107,7 @@ end
 clear windows
 
 %% Get Welch PSD estimator from the windows and store them back into records
-disp('Getting Welch PSD estimator from ECG1 windows...');
+disp('Getting Welch PSD estimator from signal windows...');
 
 recordNames = fieldnames(records);
 for recordName=recordNames'     
@@ -165,87 +152,89 @@ for recordName=recordNames'
     %disp(strcat(recordName{1}, ',', int2str(normalWindows), ',', int2str(afibWindows)));
     
     if normalWindows < minNumWindowsPerRecord || afibWindows < minNumWindowsPerRecord
-        disp(recordName{1});
+        %disp(strcat('discarded from ', recordName{1}));
         records = rmfield(records, recordName{1});
     end
 end
 
 %% Get a sample of a normal and afib window from each record, as well as their PSDs
-recordNames = fieldnames(records);
-
-normalWindowPSD = zeros(length(recordNames), size(psds, 2));  
-AFIBWindowPSD = zeros(length(recordNames), size(psds, 2));  
-recordIndex = 1;
-
-for recordName=recordNames'     
-    record = records.(recordName{1});    
-    
-    timeAxis = 0:(1.0/record.Fs):windowSizeSeconds - 1/record.Fs;
-    
-    windows = record.signalmVWindows;
-    labels = record.actualClasses;
-    psds = record.PSDs;
-        
-    normalWindowIndices = find(~labels);
-    AFIBWindowIndices = find(labels);
-    
-    % Get a normal and an AFIB window to plot as examples
-    firstNormalWindow = windows(normalWindowIndices(1), :);
-    firstAFIBWindow = windows(AFIBWindowIndices(1), :);
-    
-    % Get mean of all normal window PSDs
-    for i=1:length(normalWindowIndices)
-        normalWindowPSD(recordIndex, :) = normalWindowPSD(recordIndex, :) + psds(normalWindowIndices(i), :);
-    end
-    normalWindowPSD(recordIndex, :) = normalWindowPSD(recordIndex, :)./length(normalWindowIndices);    
-    
-    % Get mean of all AFIB window PSDs
-    for i=1:length(AFIBWindowIndices)
-        AFIBWindowPSD(recordIndex, :) = AFIBWindowPSD(recordIndex, :) + psds(AFIBWindowIndices(i), :);
-    end
-    AFIBWindowPSD(recordIndex, :) = AFIBWindowPSD(recordIndex, :)./length(AFIBWindowIndices);    
-    
-    % Plot everything
-    figure;
-    suptitle(strcat(recordName{1}));
-    
-    subplot(2, 2, 1);
-    plot(timeAxis, firstNormalWindow);
-    title('First normal window');
-    xlabel('Time (s)');
-    ylabel('Amplitude (mV)');
-    
-    subplot(2, 2, 2);
-    plot(timeAxis, firstAFIBWindow);
-    title('First AFIB window');
-    xlabel('Time (s)');
-    ylabel('Amplitude (mV)');    
-    
-    subplot(2, 2, 3:4);    
-    plot(normalWindowPSD(recordIndex, :));
-    hold on
-    plot(AFIBWindowPSD(recordIndex, :));    
-    title('Average of window PSDs');
-    xlabel('Frequency (Hz)');
-    ylabel('Power Spectral Density (dB)');    
-    legend('Normal', 'AFIB');
-end
-
-% Average all normal PSDs and all AFIB PSDs from all records
-figure;
-suptitle('Mean PSD of all windows from all records');  
-plot(mean(normalWindowPSD));
-hold on
-plot(mean(AFIBWindowPSD));    
-xlabel('Frequency (Hz)');
-ylabel('Power Spectral Density (dB)');    
-legend('Normal', 'AFIB');
-
-clear timeAxis
-clear normalWindowIndices AFIBWindowIndices
-clear psds windows labels
-clear normalWindowPSD AFIBWindowPSD
-clear firstNormalWindow firstAFIBWindow
+% recordNames = fieldnames(records);
+% 
+% normalWindowPSD = zeros(length(recordNames), size(psds, 2));  
+% AFIBWindowPSD = zeros(length(recordNames), size(psds, 2));  
+% recordIndex = 1;
+% 
+% for recordName=recordNames'     
+%     record = records.(recordName{1});    
+%     
+%     timeAxis = 0:(1.0/record.Fs):windowSizeSeconds - 1/record.Fs;
+%     
+%     windows = record.signalmVWindows;
+%     labels = record.actualClasses;
+%     psds = record.PSDs;
+%         
+%     normalWindowIndices = find(~labels);
+%     AFIBWindowIndices = find(labels);
+%     
+%     % Get a normal and an AFIB window to plot as examples
+%     firstNormalWindow = windows(normalWindowIndices(1), :);
+%     firstAFIBWindow = windows(AFIBWindowIndices(1), :);
+%     
+%     % Get mean of all normal window PSDs
+%     for i=1:length(normalWindowIndices)
+%         normalWindowPSD(recordIndex, :) = normalWindowPSD(recordIndex, :) + psds(normalWindowIndices(i), :);
+%     end
+%     normalWindowPSD(recordIndex, :) = normalWindowPSD(recordIndex, :)./length(normalWindowIndices);    
+%     
+%     % Get mean of all AFIB window PSDs
+%     for i=1:length(AFIBWindowIndices)
+%         AFIBWindowPSD(recordIndex, :) = AFIBWindowPSD(recordIndex, :) + psds(AFIBWindowIndices(i), :);
+%     end
+%     AFIBWindowPSD(recordIndex, :) = AFIBWindowPSD(recordIndex, :)./length(AFIBWindowIndices);    
+%     
+%     % Plot everything
+%     figure;
+%     suptitle(strcat(recordName{1}));
+%     
+%     subplot(2, 2, 1);
+%     plot(timeAxis, firstNormalWindow);
+%     title('First normal window');
+%     xlabel('Time (s)');
+%     ylabel('Amplitude (mV)');
+%     
+%     subplot(2, 2, 2);
+%     plot(timeAxis, firstAFIBWindow);
+%     title('First AFIB window');
+%     xlabel('Time (s)');
+%     ylabel('Amplitude (mV)');    
+%     
+%     subplot(2, 2, 3:4);    
+%     plot(normalWindowPSD(recordIndex, :));
+%     hold on
+%     plot(AFIBWindowPSD(recordIndex, :));    
+%     plot(5 * (AFIBWindowPSD(recordIndex, :) - normalWindowPSD(recordIndex, :)));
+%     title('Average of window PSDs');
+%     xlabel('Frequency (Hz)');
+%     ylabel('Power Spectral Density (dB)');    
+%     legend('Normal', 'AFIB', '5 * (AFIB - Normal)');
+% end
+% 
+% % Average all normal PSDs and all AFIB PSDs from all records
+% figure;
+% suptitle('Mean PSD of all windows from all records');  
+% plot(mean(normalWindowPSD));
+% hold on
+% plot(mean(AFIBWindowPSD));  
+% plot(5 * (mean(AFIBWindowPSD) - mean(normalWindowPSD)));
+% xlabel('Frequency (Hz)');
+% ylabel('Power Spectral Density (dB)');    
+% legend('Normal', 'AFIB', '5 * (AFIB - Normal)');
+% 
+% clear timeAxis
+% clear normalWindowIndices AFIBWindowIndices
+% clear psds windows labels
+% clear normalWindowPSD AFIBWindowPSD
+% clear firstNormalWindow firstAFIBWindow
 
 %% Delete signal and time, since we won't use them anymore
 recordNames = fieldnames(records);
@@ -256,9 +245,6 @@ end
 
 %% Extract training windows from the data
 disp('Separating training windows...');
-
-% We will use this percentage of the psds as a training set
-trainingPercentage = 0.3;
 
 recordNames = fieldnames(records);
 for recordName=recordNames'     
@@ -319,93 +305,113 @@ clear trainingClasses testClasses
 clear trainingPSDs testPSDs 
 
 %% Perform PCA of training PSDs
-% 
-% numPrincipalComponents = 0;
-% 
-% % Only perform PCA if we pick numPrincipalComponents different than zero
-% if numPrincipalComponents ~= 0
-%     disp('Performing PCA from window PSDs...');
-%     
-%     [coeff,score,latent,tsquared,explained] = pca(trainingPSDs);
-% 
-%     % We'll use this later to convert new observations into PCA components
-%     pcaMean = mean(trainingPSDs);
-%     pcaCoeffs = inv(coeff');
-% 
-%     % Get first 10 components
-%     trainingPSDsPCA = score(:, 1:numPrincipalComponents);
-% 
-%     % Map test records to PCA components
-%     recordNames = fieldnames(records);
-%     for recordName=recordNames'     
-%         record = records.(recordName{1});
-%         testPSDs = record.PSDs;        
-%         numSamples = size(testPSDs, 1);
-% 
-%         if ~record.isLearningSet       
-%             % Actually do the mapping of samples. Check test_pca for proof
-%             meanPCAmat = repmat(pcaMean, numSamples, 1);
-%             pcaTestPSDs = (testPSDs - meanPCAmat) * pcaCoeffs;
-%             pcaTestPSDs = pcaTestPSDs(:, 1:numPrincipalComponents);
-%             
-%             records.(recordName{1}).PSDs = pcaTestPSDs;
-%         end
-%     end
-% 
-%     disp('Post-PCA training set dimensions (samples x components): ');
-%     size(trainingPSDsPCA)
-%     
-%     % Overwrite our training PSDs so that the rest of the pipeline never
-%     % needs to care if we did PCA or not
-%     trainingPSDs = trainingPSDsPCA;
-% end
-
-%% Try a linear classifier instead
-disp('Classifying with a linear classifier...');
-
-totalConfMat = zeros(2, 2);
-
-recordNames = fieldnames(records);
-for recordName=recordNames'     
-    record = records.(recordName{1});    
-      
-%     % Linear classifier
-%     predictedClasses = classify(record.testPSDs, ...
-%                                 record.trainingPSDs, ...
-%                                 record.trainingClasses, ...
-%                                 'linear');
-
-    % SVM Classifier
-    SVMModel = fitcsvm(record.trainingPSDs, ...
-                       record.trainingClasses, ...
-                       'KernelFunction', ...
-                       'polynomial', ...
-                       'PolynomialOrder', ...
-                       2);
-    [predictedClasses, scoreForEachClass] = predict(SVMModel, record.testPSDs);
-                   
-    actualClasses = record.testClasses;
-
-    confMat = zeros(2, 2);        
-    confMat(1, 1) = sum(~predictedClasses & ~actualClasses);
-    confMat(2, 2) = sum(predictedClasses & actualClasses);
-    confMat(1, 2) = sum(predictedClasses & ~actualClasses);
-    confMat(2, 1) = sum(~predictedClasses & actualClasses);        
-
-%     disp(recordName{1});
-%     accuracy = (confMat(1, 1) + confMat(2, 2)) / sum(sum(confMat))
-    
-    totalConfMat = totalConfMat + confMat;
-
-    % Store prediction results into records
-    records.(recordName{1}).predictedClass = predictedClasses;
+if numPrincipalComponents == 0
+    performPCA = 0;
+else
+    performPCA = 1;
 end
 
-totalConfMat
-totalAccuracy = (totalConfMat(1, 1) + totalConfMat(2, 2)) / sum(sum(totalConfMat))
+tic; % Start counting time
 
+if performPCA
+    disp('Performing PCA from window PSDs...');    
+    
+    % Map test records to PCA components
+    recordNames = fieldnames(records);
+    for recordName=recordNames'     
+        record = records.(recordName{1});
+        
+        [coeff,score,~,~,~] = pca(record.trainingPSDs);
+        
+        % Actually do the mapping of samples. Check test_pca for proof
+        numTestPSDs = size(record.testPSDs, 1);
+        meanPCAmat = repmat(mean(record.trainingPSDs), numTestPSDs, 1);
+        pcaTestPSDs = (record.testPSDs - meanPCAmat) / coeff';
+        
+        % Update our PSDs
+        records.(recordName{1}).trainingPSDs = score;
+        records.(recordName{1}).testPSDs = pcaTestPSDs;        
+    end
+end
 
+clear coeff score
+clear numTestPSDs meanPCAmat pcaTestPSDs
 
+%% Try a linear classifier instead
+disp('Training and applying classifier...');
+
+% Make sure we don't clip our PSDs if we didn't choose PCA
+if ~performPCA
+    recordNames = fieldnames(records);  
+    recordName = recordNames(1);
+    numPrincipalComponents = size(records.(recordName{1}).testPSDs, 2);
+end
+
+recordNames = fieldnames(records);
+numSignals = length(recordNames);
+signalIndex = 1;
+
+sensitivities = zeros(numSignals, 1);
+specificities = zeros(numSignals, 1);
+PPVs = zeros(numSignals, 1);
+
+for recordName=recordNames'       
+    record = records.(recordName{1});
+    
+    actualClasses = record.testClasses;
+    predictedClasses = actualClasses;
+    
+    if strcmp(classifier, 'linear')
+        predictedClasses = classify(record.testPSDs(:, 1:numPrincipalComponents), ...
+                                    record.trainingPSDs(:, 1:numPrincipalComponents), ...
+                                    record.trainingClasses, ...
+                                    'linear');
+    elseif strcmp(classifier, 'svm_linear')
+        SVMModel = fitcsvm(record.trainingPSDs(:, 1:numPrincipalComponents), ...
+                           record.trainingClasses, ...
+                           'KernelFunction', ...
+                           'linear');
+        [predictedClasses, ~] = predict(SVMModel, record.testPSDs(:, 1:numPrincipalComponents));
+    elseif strcmp(classifier, 'svm_rbf')
+        SVMModel = fitcsvm(record.trainingPSDs(:, 1:numPrincipalComponents), ...
+                           record.trainingClasses, ...
+                           'KernelFunction', ...
+                           'rbf');
+        [predictedClasses, ~] = predict(SVMModel, record.testPSDs(:, 1:numPrincipalComponents));
+    elseif strcmp(classifier, 'svm_poly')
+        SVMModel = fitcsvm(record.trainingPSDs(:, 1:numPrincipalComponents), ...
+                           record.trainingClasses, ...
+                           'KernelFunction', ...
+                           'Polynomial', ...
+                           'PolynomialOrder', ...
+                           '3');
+        [predictedClasses, ~] = predict(SVMModel, record.testPSDs(:, 1:numPrincipalComponents));
+    elseif strcmp(classifier, 'fitclinear')
+        SVMModel = fitclinear(record.trainingPSDs(:, 1:numPrincipalComponents), ...
+                              record.trainingClasses);
+        [predictedClasses, ~] = predict(SVMModel, record.testPSDs(:, 1:numPrincipalComponents));
+    end
+        
+    confMat = zeros(2, 2);        
+    confMat(1, 1) = sum(~predictedClasses & ~actualClasses); %TN
+    confMat(2, 2) = sum(predictedClasses & actualClasses); %TP
+    confMat(1, 2) = sum(predictedClasses & ~actualClasses); %FP
+    confMat(2, 1) = sum(~predictedClasses & actualClasses); %FN    
+
+    sensitivity = confMat(2, 2) / (confMat(2, 1) + confMat(2, 2));
+    specificity = confMat(1, 1) / (confMat(1, 1) + confMat(1, 2));
+    ppv = confMat(2, 2) / (confMat(2, 2) + confMat(1, 2)) ;
+        
+    sensitivities(signalIndex) = sensitivity;
+    specificities(signalIndex) = specificity;
+    PPVs(signalIndex) = ppv;
+    
+    signalIndex = signalIndex + 1;
+end
+
+results = {classifier, toc, trainingPercentage, numRecordsToClassify, size(sensitivities, 1), numPrincipalComponents, mean(sensitivities), mean(specificities), mean(PPVs), std(sensitivities), std(specificities), std(PPVs)};
+
+end
 
 
 
